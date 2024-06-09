bb2f29a181455a73164c923bc30d384b92f5d341
Andro <92311039+Rayhandro@users.noreply.github.com>
2024-06-10 00:27:21 +0700

    Update Get Balance

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -7,7 +7,8 @@ const {
   createNewTransaction,
   editTransaction,
   deleteTransaction,
-  getTransactionsByWeek
+  getTransactionsByWeek,
+  getBalance
 } = require('../controllers/transactionControllers');
 
 const router = express.Router();
@@ -18,5 +19,6 @@ router.post('/', verifyToken, createNewTransaction);
 router.put('/edit', verifyToken, editTransaction);
 router.delete('/delete', verifyToken, deleteTransaction);
 router.get('/week', verifyToken, getTransactionsByWeek);
+router.get('/balance', verifyToken, getBalance);
 
 module.exports = router;
